b4005e3882381f89e5bd5f0334e3200d9109179e
musafir ranjha <CRYPTOBYAASHISH@GMAIL.COM>
2024-08-03 13:13:17 +0000

    feat: initiated the project

--- contracts/PrivateNFT.sol
+++ contracts/PrivateNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("cocobar","bar")
+        ERC721("solobar","solo")
         Ownable(initialOwner)
     {}
 
